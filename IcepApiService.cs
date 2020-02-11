644ce8358b403b0fc633d2c0e0f021b24a9f7067
Rebecca <rebecca.rangel@c2es.com.br>
2020-02-11 10:31:07 -0300

    api_cep

---
+++ IcepApiService.cs
@@ -0,0 +1,13 @@
+﻿
+
+using Refit;
+using System.Threading.Tasks;
+
+namespace API_CEP
+{
+    interface ICepApiService
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepResponse> GetAddressAsync(string cep);
+    }
+}
